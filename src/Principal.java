07571ccf67b13502a100f4bf9ea01b8492b0bc67
Martin <martinjurado572@gmail.com>
2026-04-08 11:54:17 -0500

    Se agregaron setters y mejoras en la clase Libro

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
+
         Libro l1 = new Libro(3, "Carlo Collodi", "Cuento", "Pinocho", 1881);
         Libro l2 = new Libro();
 
@@ -23,7 +24,15 @@ public class Principal {
         System.out.println("Libro 1: " + l1.mostrarInformacion());
         System.out.println("Libro 2: " + l2.mostrarInformacion());
 
+        // set
+        System.out.println("Modificando datos del libro...");
+
         l1.setTitulo("El principito");
-        System.out.println("Libro 1: " + l1.mostrarInformacion());
+        l1.setAutor("Antoine de Saint-Exupéry");
+        l1.setCategoria("Fábula");
+        l1.setAnio(1943);
+        l1.setCantidadEjemplares(10);
+
+        System.out.println("Libro 1 modificado: " + l1.mostrarInformacion());
     }
 }
