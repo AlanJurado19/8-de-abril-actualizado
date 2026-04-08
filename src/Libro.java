5f438d6c598e9c1f8a5d4bd281dbd100adc35bb1
Martin <martinjurado572@gmail.com>
2026-04-08 11:26:40 -0500

    Primer commit - proyecto biblioteca

---
+++ src/Libro.java
@@ -0,0 +1,51 @@
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private int anio;
+    private int cantidadEjemplares;
+
+    public Libro(){
+        titulo ="Java para ingeniros";
+        autor="Paulo Guerra";
+        categoria="Programacion";
+        anio=2014;
+        cantidadEjemplares=5;
+
+    }
+
+    public Libro(int cantidadEjemplares,
+                 String autor,String categoria, String titulo, int anio){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.categoria=categoria;
+        this.anio=anio;
+        this.cantidadEjemplares=cantidadEjemplares;
+
+    }
+
+    public String mostrarInformacion(){
+        String aux="";
+        aux="Titulo: "+titulo+"\n";
+        aux+="Autor: "+autor+"\n";
+        aux+="Categoria: "+categoria+"\n";
+        aux+="Año: "+anio+"\n";
+        aux+="Cantidad: "+cantidadEjemplares+" ejemplares(es) \n";
+        return aux;
+
+    }
+
+    public int prestar(){
+        if(cantidadEjemplares>0){
+            cantidadEjemplares--;
+            return cantidadEjemplares;
+        }
+        return -1;  // Significa que no no hay ejemlares
+
+    }
+    public void setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,29 @@
+public class Principal {
+    public static void main(String[] args) {
+        Libro l1 = new Libro(3, "Carlo Collodi", "Cuento", "Pinocho", 1881);
+        Libro l2 = new Libro();
+
+        System.out.println("Libro 1: " + l1.mostrarInformacion());
+        System.out.println();
+
+        if (l1.prestar() < 0) {
+            System.out.println("No hay ejemplares");
+        } else {
+            System.out.println("Ejemplar prestado");
+        }
+
+        System.out.println("Prestando libro...");
+
+        if (l1.prestar() < 0) {
+            System.out.println("No hay ejemplares");
+        } else {
+            System.out.println("Ejemplar prestado");
+        }
+
+        System.out.println("Libro 1: " + l1.mostrarInformacion());
+        System.out.println("Libro 2: " + l2.mostrarInformacion());
+
+        l1.setTitulo("El principito");
+        System.out.println("Libro 1: " + l1.mostrarInformacion());
+    }
+}
